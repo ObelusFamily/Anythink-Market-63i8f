227d8d6b7a9729d35c23bde7caa7f3b4d057acdb
Francis Turmel <fturmel@gmail.com>
2022-09-13 17:44:10 -0400

    Format backend JS files with Prettier

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,22 +1,22 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 
 var CommentSchema = new mongoose.Schema(
-  {
-    body: String,
-    seller: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    item: { type: mongoose.Schema.Types.ObjectId, ref: "Item" }
-  },
-  { timestamps: true }
+	{
+		body: String,
+		seller: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+		item: { type: mongoose.Schema.Types.ObjectId, ref: 'Item' },
+	},
+	{ timestamps: true },
 );
 
 // Requires population of seller
-CommentSchema.methods.toJSONFor = function(user) {
-  return {
-    id: this._id,
-    body: this.body,
-    createdAt: this.createdAt,
-    seller: this.seller.toProfileJSONFor(user)
-  };
+CommentSchema.methods.toJSONFor = function (user) {
+	return {
+		id: this._id,
+		body: this.body,
+		createdAt: this.createdAt,
+		seller: this.seller.toProfileJSONFor(user),
+	};
 };
 
-mongoose.model("Comment", CommentSchema);
+mongoose.model('Comment', CommentSchema);
