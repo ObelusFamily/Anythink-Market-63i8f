75a928506848a08ab99e376f8cb2b0a06a0c99a0
Francis Turmel <fturmel@gmail.com>
2022-09-26 11:28:27 -0400

    Display placeholder image if missing

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -1,7 +1,7 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import agent from "../agent";
-import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
 
 const mapDispatchToProps = (dispatch) => ({
@@ -29,6 +29,8 @@ const ItemPreview = (props) => {
     }
   };
 
+  const image = item.image || "/placeholder.png";
+
   return (
     <div
       className="card bg-dark border-light p-3"
@@ -36,7 +38,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
